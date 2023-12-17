cd3465290c137c41ae478ef685615cd109b3396b
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2023-12-17 14:31:30 +0000

    Dice iOS13 - 기본 세팅

---
+++ Dice-iOS/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+         print("Click!!")
+    }
+    
+}
+

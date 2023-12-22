698ece73dbabc59ae37342c9d552af0f54a18583
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2023-12-22 01:19:43 +0000

    [코딩 연습] 함수(Functions) Part 2

--- MyPlayground.swift
+++ MyPlayground.swift
@@ -168,7 +168,7 @@ func exercise() {
 
 /*
 ***************************************************************************************
-  [Function and Scope]
+  [Function - Functions and Scope]
 ***************************************************************************************  
 */
 
@@ -221,3 +221,107 @@ right()
 right()
 right()
 right()
+
+
+
+/*
+***************************************************************************************
+  [Function - Functions with Inputs and Type Inference ]
+***************************************************************************************  
+*/
+var myAge: String = "This data type is String"
+
+// How to declare a function
+// func FuncName (parameter: DataType){ }
+// func 함수이름 (매개변수이름: 데이터 타입) {}
+func greeting3 (whoToGreet: String) {
+  print("Hello, \(whoToGreet)!") // Hello, happy!
+}
+
+// 인수: 함수를 호출하거나 트리거 할 때 전달되는 값 (여기서는 "happy")
+greeting3(whoToGreet: "happy")
+
+
+
+/*
+[Coding Exercise] Functions Part 2
+In this coding exercise, you're going to create a simple calculator. 
+// 이 연습에서는 간단한 계산기를 만들 것입니다.
+You will need to create 4 functions so that when you run the code, a and b are added/subtracted/multiplied and divided by each other.
+// 코드를 실행하면 a와 b가 서로 더하기/빼기/곱하기/나누기가 되도록 4개의 함수를 만들어야 합니다.
+
+To test your code, you can add some inputs using the button in the top right corner of the console, 
+shown in the example below:
+
+It will ask you for some input values. 
+The values need to be numbers and they will be substituted into your code as the variables a and b. 
+// 몇가지 입력값을 묻는 메시지가 표시됩니다. 값은 숫자여야 하며 그것들은 변수 a와 b로 대체되었습니다.
+The numbers will need to be on separate lines.
+// 숫자들은 별도의 층에 표시되어야 합니다.
+
+e.g. If
+a = 3 and b = 4
+This is what you would expect to be printed from the calculator:
+계산기에서 출력되는 예상되는 내용은 다음과 같습니다.
+
+7
+-1
+12
+0.75
+Because:
+
+3 + 4 = 7,
+3 - 4 = -1,
+3 x 4 = 12,
+3 ÷ 4 = 0.75
+
+You can try out your code here: https://repl.it/@appbrewery/functions-2-exercise#main.swift
+
+NOTE: For the solution to pass the tests, you cannot alter any of the existing code below. 
+a and b will come from the testing code. You should not modify them here. 
+All you need to do is write the 4 functions. Also, there cannot be anything else 
+printed in the console other than the 4 solutions as per the example above.
+
+Did you get 0 for the last value? Hint: you'll need to remind yourself about 
+Swift data types and maybe this link to Google will help.
+*/
+
+//Don't change this code:
+func calculator() {
+  let a = Int(readLine()!)! //First input
+  let b = Int(readLine()!)! //Second input
+  
+  add(n1: a, n2: b)
+  subtract(n1: a, n2: b)
+  multiply(n1: a, n2: b)
+  divide(n1: a, n2: b)
+  
+}
+
+//Write your code below this line to make the above function calls work.
+func add(n1: Int, n2: Int) {
+  print(n1 + n2)
+}
+func subtract(n1: Int, n2: Int) {
+  print(n1 - n2)
+}
+func multiply(n1: Int, n2: Int) {
+  print(n1 * n2)
+}
+
+// func divide is must use Double or Float
+func divide(n1: Int, n2: Int) {
+  let a = Double(n1) // 3.0
+  let b = Double(n2) // 4.0
+  print(a / b) // 0.75
+
+  // if Double is not used, result will be zero
+  // print(a / b) // 0
+}
+
+//Don't move or change this code:
+calculator(n1: 3, n2: 4)
+// 7
+// -1
+// 12
+// 0.75

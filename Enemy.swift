74dc18fa6edb24c869c72a596f029d5e0a409161
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2023-12-29 12:39:04 +0000

    [학습] Struct vs Class

--- Enemy.swift
+++ Enemy.swift
@@ -1,14 +1,31 @@
 /*
 ***************************************************************************************
-  [Enemy Class]
+  [Enemy - Class]
 ***************************************************************************************  
 */
+
+/*
+
 // Swift에서 class로 된 파일을 만들 시 파일의 이름과 클래스의 이름이 일치해야 함
 // 적 Class
 class Enemy {
   // 속성
-  var hearth = 100 // 생명력
-  var attackStrength = 10 // 적 공격력
+  // var health = 100 // 생명력
+  // var attackStrength = 10 // 적 공격력
+
+  // class는 초기화 프로그램(init)도 있으므로 기본값을 삭제하고 데이터 타입으로만 간단히 지정할 수 있다.
+  var health: Int
+  var attackStrength: Int
+  // 초기화
+  init(health: Int, attackStrength: Int){
+    self.hearth = health
+    self.attackStrength = attackStrength
+  }
+
+  // 받은 피해
+  func takeDamage(amount: Int) {
+    health = health - amount
+  }
 
   // 움직임
   func move() {
@@ -25,3 +42,41 @@ class Enemy {
   구조체(struct)와 다른 점
   class는 SuperClass를 상속받는 기능이 있다.
 */
+
+*/
+
+/*
+***************************************************************************************
+  [Enemy - Struct]
+*************************************************************************************** 
+*/
+
+struct Enemy {
+  // 속성
+  // var health = 100 // 생명력
+  // var attackStrength = 10 // 적 공격력
+
+  // class는 초기화 프로그램(init)도 있으므로 기본값을 삭제하고 데이터 타입으로만 간단히 지정할 수 있다.
+  var health: Int
+  var attackStrength: Int
+  // 초기화
+  init(health: Int, attackStrength: Int){
+    self.hearth = health
+    self.attackStrength = attackStrength
+  }
+
+  // 받은 피해
+  mutating func takeDamage(amount: Int) {
+    health = health - amount
+  }
+
+  // 움직임
+  func move() {
+    print("Walk forwards")
+  }
+
+  // 공격
+  func attack() {
+    print("Land a hit, does \(attackStrength)")
+  }
+}
--- main.swift
+++ main.swift
@@ -3,21 +3,49 @@
   [메인화면: Enemy를 통해 만들어진 적들이 있다.]
 ***************************************************************************************  
 */
+
+/*
+***************************************************************************************
+  class를 사용한 Enemy
+***************************************************************************************  
+*/
 // class 초기화는 class 이름 뒤에 괄호를 쳐주면 된다. Enemy()
 
 // Enemy class로 만든 적(skeleton)
-let skeleton = Enemy()
-print(skeleton.hearth) // 100
-skeleton.move()
-skeleton.attack()
+  // let skeleton1 = Enemy(health: 100, attackStrength: 10)
+// skeleton1을 skeleton2에 복사한 것처럼 보이지만,
+// 그저 같은 참조를 하고 있을 뿐이다.(동일한 객체를 참조함)
+  // let skeleton2 = skeleton1
 
-let skeleton2 = Enemy()
-let skeleton3 = Enemy()
+  // skeleton1.takeDamage(amount: 10)
+  // print(skeleton2.health) // 90
+// => skeleton1만 10을 뺐을 뿐인데 skeleton2도 값이 빠지는 이유?
+// 클래스는 '참조'로 전달되기 때문
 
+
+/*
 // Enemy class로 만든 적(Dragon)
-let dragon = Dragon()
-dragon.wingSpan = 5 // 드래곤 클래스 안에서만 존재하는 사용자 지정 속성
-dragon.hearth = 15
-dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are a hurricane!!!")
-dragon.move()
-dragon.attck()
+  let dragon = Dragon()
+  dragon.wingSpan = 5 // 드래곤 클래스 안에서만 존재하는 사용자 지정 속성
+  dragon.health = 15
+  dragon.talk(speech: "My teeth are swords! My claws are spears! My wings are a hurricane!!!")
+  dragon.move()
+  dragon.attck()
+*/
+
+/*
+***************************************************************************************
+  struct 를 사용한 Enemy
+***************************************************************************************  
+*/
+
+var skeleton1 = Enemy(health: 100, attackStrength: 10)
+var skeleton2 = skeleton1
+
+skeleton1.takeDamage(amount: 10)
+skeleton1.takeDamage(amount: 10)
+skeleton2.takeDamage(amount: 10)
+
+// struct 를 통한 복사이기 때문에 각각 다른 개체를 참조한다.
+print(skeleton1.health) // 80
+print(skeleton2.health) // 90

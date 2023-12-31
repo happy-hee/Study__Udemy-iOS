689fd24e7f6862a63580a35b2d464f87bf14ad3c
Happy Hee <56466589+happy-hee@users.noreply.github.com>
2023-12-31 21:51:55 +0000

    [학습] 옵셔널 바인딩, 체이닝, nil 병합 연산자

--- 08_Optionals.swift
+++ 08_Optionals.swift
@@ -55,3 +55,92 @@ func highestScore(scores: [String: Int]) {
     print(maxScore)
   }
 }
+
+
+
+/*
+***************************************************************************************
+  [Optional Binding, Chaining, and the Nil Coalescing Operator]
+***************************************************************************************  
+*/
+/*
+1. Force Unwrapping  // 강제 언래핑 
+   강제적으로 언래핑. 위험성이 있으므로 가능하면 사용하지 않는다.
+*/
+optional!
+
+
+/*
+2. Check for nil value // nil 값 확인 
+    if 문 사용 => 통해 해당 옵셔널이 nil 인지 확인하고,
+    만약 아니라면 언래핑을 한다.
+*/
+if optional != nil {
+  optional!
+}
+
+
+/*
+3. Optional Binding // 옵셔널 바인딩
+    if let 문 사용 => nil 이 아닌 경우, 새 상수에 옵션의 값을 할당한다.
+    [형태]
+    if let safeOptional = optional {
+      safeOptional
+    }
+*/
+let myOptional1: String? // String 타입과 String?(옵셔널String) 타입은 완전히 다른 데이터 유형을 가지고 있다.
+myOptional1 = "Alban" // Optional("Alban")
+
+let text: String = myOptional1 // Error (myOptional1은 옵셔널String 이기때문에)
+
+// if let 을 사용한 옵셔널 바인딩
+if let safeOptional = myOptional1 {
+  let text: String = safeOptional
+  let text2: String = safeOptional
+  print(safeOptional)
+} else { // safeOptional이 nil 이면 아래 코드 실행
+  print("myOptional1 was found to be nil.")
+}
+
+
+/*
+4. Nill Coalescing Operator // nil 병합 연산자
+    [형태]
+    optional ?? 기본값
+*/
+let myOptional2: String?
+myOptional2 = nil
+// 두개의 물음표를 사용 => 옵셔널 값이 nil일 경우 뒤의 기본값을 넣는다.
+// 옵셔널 값이 nil이 아닐 경우는 해당 값을 넣는다.
+let text3: String = myOptional2 ?? "I am the default value"
+print(text3) // myOptional2가 nil 이므로 기본값 출력 ( I am the default value )
+
+// 만약 옵셔널 값이 있을 경우(nil이 아닌 경우)
+// myOptional2 = "값이 있어요"
+// let text3: String = myOptional2 ?? "I am the default value"
+// print(text3) // 값이 있어요
+
+
+/*
+5. Optional Chaining // 옵셔널 체이닝
+    [형태]
+    optional?.property
+    optional?.method()
+*/
+struct MyOptionalStruct {
+  var myProperty = 123
+  func myMethod() {
+    print("I am the MyOptionalStruct struct's method")
+  }
+}
+
+let myOptional3 : MyOptionalStruct?
+myOptional3 = MyOptionalStruct()
+
+print(myOptional3.myProperty) // Error 
+
+// 속성은 선택 사항이 아니지만 'struct가 옵셔널' 이기 때문에 해당 속성을 사용하려면 언래핑을 해야한다.
+// 옵셔널 체이닝(?.)을 사용해서 안전하게 언래핑
+// => 옵션이 nil이 아니면 이 사슬을 따라 해당 속성에 접근한다.
+print(myOptional3?.myProperty) // Optional(123)
+print(myOptional3?.myMethod()) // I am the MyOptionalStruct struct's method
